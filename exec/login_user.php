f7d08c6041a7f163caee55f8186b0dfcc8ac1e48
Peter Buzanits <p.buzanits@epb.at>
2022-01-25 17:22:16 +0100

    fix paths

--- exec/login_user.php
+++ exec/login_user.php
@@ -1,7 +1,7 @@
 <?php
 namespace booosta\usersystem;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use booosta\Framework as b;
 b::croot();
